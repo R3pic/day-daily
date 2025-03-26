853e334ba2110882e73baf54bc7a954a9274fac0
R3pic <101osc@naver.com>
2025-03-26 16:17:05 +0900

    feat: ConfigModule 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { validate } from '@common/env';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
