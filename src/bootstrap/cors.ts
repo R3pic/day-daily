c2d2ab568813020d844af407a40b8fbed47169e5
R3pic <101osc@naver.com>
2025-04-17 19:32:34 +0900

    fix: cors origin

--- src/bootstrap/cors.ts
+++ src/bootstrap/cors.ts
@@ -2,7 +2,7 @@ import { INestApplication } from '@nestjs/common';
 
 export function setUpCors(app: INestApplication) {
   app.enableCors({
-    origin: ['https://daisy.wisoft.io/yehwan/app1'],
+    origin: ['http://localhost:5174','https://daisy.wisoft.io/yehwan/app1'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
