f838ad06b14119239b92e68a3e1b0fe90e065edd
R3pic <101osc@naver.com>
2025-04-24 19:17:09 +0900

    swagger: 옵셔널 옵션 추가

--- src/common/dto/pagination-query.dto.ts
+++ src/common/dto/pagination-query.dto.ts
@@ -5,12 +5,14 @@ export class PaginationQuery {
 
   @ApiProperty({
     description: '몇개를 건너뛸건지',
+    required: false,
   })
   @IsOptional()
   offset?: number;
 
   @ApiProperty({
     description: '몇개를 선택할건지',
+    required: false,
   })
   @IsOptional()
   limit?: number;
