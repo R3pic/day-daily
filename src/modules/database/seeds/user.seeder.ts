b026286a44ec1187e047b906adc43f7a8379a8ba
R3pic <101osc@naver.com>
2025-06-30 20:12:59 +0900

    fix: user seed plain

--- src/modules/database/seeds/user.seeder.ts
+++ src/modules/database/seeds/user.seeder.ts
@@ -30,7 +30,8 @@ export default class UserSeeder implements Seeder {
         email: 'test@email.com',
         fullName: '홍길동',
         nickname: '길동이',
-        password: '$2b$10$0ilrBObGGuoKrpOZ849EOevPcp.5wqES4bqtRMkJh6iQAqLMSxZ2q',
+        // Plain : Test123$
+        password: '$2b$10$GEMlvu/Lw//RSqjbxdZBBO9ZcFK1E02FADnlGGrmaM19MLO2o.H4K',
       }))
       .orIgnore(true)
       .execute();
