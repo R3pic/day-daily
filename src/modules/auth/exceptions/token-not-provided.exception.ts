d2efa2493b925a1864f0aaa72ee307de418bb24a
R3pic <101osc@naver.com>
2025-07-23 17:57:38 +0900

    fix: 에러코드

--- src/modules/auth/exceptions/token-not-provided.exception.ts
+++ src/modules/auth/exceptions/token-not-provided.exception.ts
@@ -4,6 +4,6 @@ import { ErrorCode } from '@common/constants/error-code';
 
 export class TokenNotProvidedException extends ServiceException {
   constructor() {
-    super(HttpStatus.UNAUTHORIZED, '토큰이 존재하지 않습니다.', ErrorCode.TOKEN_EXPIRED);
+    super(HttpStatus.UNAUTHORIZED, '토큰이 존재하지 않습니다.', ErrorCode.TOKEN_NOT_PROVIDED);
   }
 }
