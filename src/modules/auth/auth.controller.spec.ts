e4cbf19ba5ed789f382b3ba9130c4cd76a4036f0
R3pic <101osc@naver.com>
2025-10-15 12:03:34 +0900

    fix: unused variables

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -4,7 +4,6 @@ import { AuthService } from './auth.service';
 import { mock, MockProxy } from 'jest-mock-extended';
 import { RegisterDto } from '@auth/dto';
 import { RequestUser } from '@common/dto';
-import { Response } from 'express';
 
 describe('AuthController', () => {
   let controller: AuthController;
