d623dcd8b1f0658a5339c3c427b59be59ffa2c7c
R3pic <101osc@naver.com>
2025-10-15 12:05:13 +0900

    chore: add host

--- src/bootstrap/cors.ts
+++ src/bootstrap/cors.ts
@@ -2,7 +2,7 @@ import { INestApplication } from '@nestjs/common';
 
 export function setUpCors(app: INestApplication) {
   app.enableCors({
-    origin: ['http://localhost:5174','https://daisy.wisoft.io'],
+    origin: ['http://localhost:5174','https://daisy.wisoft.io', 'https://d37p877plmv8u.cloudfront.net'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
